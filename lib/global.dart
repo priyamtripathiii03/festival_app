7a468e19af892d435764fd8e8ab0fe0ed596df8b
priyamtripathiii03 <priyamtripathiii03@gmail.com>
2024-09-13 13:37:00 +0530

    Initial commit

---
+++ lib/global.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+List festivalList=[
+  {
+'Festival name' : 'Makar Sakranti',
+'Text': 'Create your Makar Sakranti Templates',
+'Images' : 'assets/images/m1.jpeg',
+'Color' :Colors.teal,
+'image' : [
+'assets/images/makarsankranti/m1.jpeg',
+'assets/images/makarsankranti/m2.jpeg',
+'assets/images/makarsankranti/m3.jpeg',
+'assets/images/makarsankranti/m4.jpeg',
+'assets/images/makarsankranti/m5.jpeg',
+'assets/images/makarsankranti/m6.jpeg',
+'assets/images/makarsankranti/m7.jpeg',
+]
+},
+  {
+    'Festival name' : 'Vasant Panchami',
+    'Text': 'Create your Vasant Panchami Templates',
+    'Images' : 'assets/images/v1.jpeg',
+    'Color' :Colors.green,
+    'image' : [
+      'assets/images/vasant panchami/v1.jpeg',
+      'assets/images/vasant panchami/v2.jpeg',
+      'assets/images/vasant panchami/v3.jpeg',
+      'assets/images/vasant panchami/v4.jpeg',
+      'assets/images/vasant panchami/v5.jpeg',
+      'assets/images/vasant panchami/v6.jpeg',
+      'assets/images/vasant panchami/v7.jpeg',
+    ]
+
+  }
+];
